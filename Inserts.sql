6477ffcce75df332702beefb3c7702dcad981b90
Cassimiroardo <56744175+Cassimiroardo@users.noreply.github.com>
2019-10-30 20:29:49 -0300

    Update Inserts.sql

--- Inserts.sql
+++ Inserts.sql
@@ -9,25 +9,6 @@ INSERT INTO localizacao VALUES
 (6,'f',6,'f','f',6,6,6,'f'),
 (7,'g',7,'g','g',7,7,7,'g');
 
-
-INSERT INTO agenda VALUES
-(1,null,null),
-(2,null,null),
-(3,null,null),
-(4,null,null),
-(5,null,null),
-(6,null,null),
-(7,null,null),
-
-(8,null,null),
-(9,null,null),
-(10,null,null),
-(11,null,null),
-(12,null,null),
-(13,null,null),
-(14,null,null);
-
-
 INSERT INTO foto VALUES 
 (1,null,null,'/foto1.jpg'),
 (2,null,null,'/foto2.jpg'),
@@ -47,14 +28,13 @@ INSERT INTO foto VALUES
 ;
 
 INSERT INTO banda VALUES 
-(1,1,'a@g.com',1,4,'a','123'),
-(2,2,'b@g.com',2,4,'b','123'),
-(3,3,'c@g.com',3,4,'c','123'),
-(4,4,'d@g.com',4,4,'d','123'),
-(5,5,'e@g.com',5,4,'e','123'),
-(6,6,'f@g.com',6,4,'f','123'),
-(7,7,'g@g.com',7,4,'g','123');
-
+(1,'a@g.com',1,null,4,'a','123'),
+(2,'b@g.com',2,null,4,'b','123'),
+(3,'c@g.com',3,null,4,'c','123'),
+(4,'d@g.com',4,null,4,'d','123'),
+(5,'e@g.com',5,null,4,'e','123'),
+(6,'f@g.com',6,null,4,'f','123'),
+(7,'g@g.com',7,null,4,'g','123');
 
 INSERT INTO avaliacao_banda VALUES
 (1,1,4,4,date('2003-12-31 14:00:00')),
@@ -65,16 +45,30 @@ INSERT INTO avaliacao_banda VALUES
 (6,6,5,1,date('2003-12-31 16:00:00')),
 (7,7,2,2,date('2003-12-31 17:30:00'));
 
-
 INSERT INTO estudio VALUES 
-(1,7,1,'ea@g.com',7,1,'meia-boca','123'),
-(2,8,2,'eb@g.com',8,2,'do Paulo','123'),
-(3,9,3,'ec@g.com',9,3,'c','123'),
-(4,10,4,'ed@g.com',10,4,'d','123'),
-(5,11,5,'ee@g.com',11,5,'e','123'),
-(6,12,6,'ef@g.com',12,6,'f','123'),
-(7,13,7,'eg@g.com',13,7,'g','123');
+(1,null,1,'ea@g.com',7,1,'meia-boca','123'),
+(2,null,2,'eb@g.com',8,2,'do Paulo','123'),
+(3,null,3,'ec@g.com',9,3,'c','123'),
+(4,null,4,'ed@g.com',10,4,'d','123'),
+(5,null,5,'ee@g.com',11,5,'e','123'),
+(6,null,6,'ef@g.com',12,6,'f','123'),
+(7,null,7,'eg@g.com',13,7,'g','123');
 
+INSERT INTO reserva VALUES 
+(1,1,1, DATE('2003-12-31'),TIME('13:00:00'),TIME('14:00:00'),12),
+(2,2,2, DATE('2003-12-31'),TIME('14:30:00'),TIME('15:30:00'),20.2),
+(3,3,3,date('2003-12-31'),time('13:00:00'),time('14:00:00'),123.1),
+(4,4,4,date('2003-12-31'),time('11:00:00'),time('12:00:00'),32.3),
+(5,5,5,date('2003-12-31'),time('13:00:00'),time('15:30:00'),29),
+(6,6,6,date('2003-12-31'),time('13:00:00'),time('16:00:00'),1),
+(7,7,7,date('2003-12-31'),time('13:00:00'),time('17:30:00'),0.6),
+(8,7,7, DATE('2003-12-31'),TIME('13:00:00'),TIME('14:00:00'),12),
+(9,6,6, DATE('2003-12-31'),TIME('14:30:00'),TIME('15:30:00'),20.2),
+(10,5,5,date('2003-12-31'),time('13:00:00'),time('14:00:00'),123.1),
+(11,4,4,date('2003-12-31'),time('11:00:00'),time('12:00:00'),32.3),
+(12,3,3,date('2003-12-31'),time('13:00:00'),time('15:30:00'),29),
+(13,2,2,date('2003-12-31'),time('13:00:00'),time('16:00:00'),1),
+(14,1,1,date('2003-12-31'),time('13:00:00'),time('17:30:00'),0.6);
 
 INSERT INTO avaliacao_estudio VALUES
 (1,1,5,5,5,5,date('2003-12-31 14:00:00')),
@@ -85,18 +79,6 @@ INSERT INTO avaliacao_estudio VALUES
 (6,6,5,5,5,5,date('2003-12-31 16:00:00')),
 (7,7,5,5,5,5,date('2003-12-31 17:30:00'))
 ;
-
-
-INSERT INTO reserva VALUES 
-(1,1,1, DATE('2003-12-31'),TIME('13:00:00'),TIME('14:00:00')),
-(2,1,2, DATE('2003-12-31'),TIME('14:30:00'),TIME('15:30:00')),
-
-(3,3,3,date('2003-12-31'),time('13:00:00'),time('14:00:00')),
-(4,4,4,date('2003-12-31'),time('11:00:00'),time('12:00:00')),
-(5,5,5,date('2003-12-31'),time('13:00:00'),time('15:30:00')),
-(6,6,6,date('2003-12-31'),time('13:00:00'),time('16:00:00')),
-(7,7,7,date('2003-12-31'),time('13:00:00'),time('17:30:00'));
-
 
 INSERT INTO chat VALUES 
 (1,1,1),
@@ -137,3 +119,4 @@ INSERT INTO subservico VALUES
 (5,'ABCssssD', 5),
 (6,'ABCDd',    6),
 (7,'ABdasCD', 7);
+
